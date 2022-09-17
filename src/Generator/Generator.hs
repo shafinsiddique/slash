5b32c8d21b41d44139eef86b194dbcf4f3cea89f
Shafin Siddique <siddiqueshafin20@gmail.com>
2022-09-17 14:40:25 -0400

    code gen helper functions, x86 data types.

--- src/Generator/Generator.hs
+++ src/Generator/Generator.hs
@@ -3,11 +3,21 @@ import Parser.ProgramNode
 import Generator.X86Assembly 
 import Text.Printf
 
-getPrintAsm :: String -> X86Assembly 
-getPrintAsm str = let variableName = printf "const.1" in  
+getInitialAsm :: X86Assembly 
+getInitialAsm = let codeSection = [Global "_main", Default "rel", Extern "printf"] in
+    X86Assembly {dataSection = [], codeSection = codeSection}
+
+getDataConstName :: X86Assembly -> String
+getDataConstName (X86Assembly {dataSection = dataSection}) = 
+                        printf "const_%s" (show (length dataSection))
+                        
+getPrintAsm :: String -> X86Assembly -> X86Assembly 
+getPrintAsm str existing = let variableName = getDataConstName existing in  
     let dataSection = [X86Data {variableName = variableName, value = str, end = 0xA}] in 
         let codeSection = [MOV RDI variableName, Call "_printf"] in 
-            X86Assembly {dataSection = dataSection, codeSection = codeSection}
+            mergeAsm existing (X86Assembly {dataSection = dataSection, codeSection = codeSection})
 
 generateX86 :: Expression -> X86Assembly
-generateX86 PrintExpr {toPrint = str} = getPrintAsm str
+generateX86 expression = let initialAsm = getInitialAsm in 
+    case expression of
+        PrintExpr {toPrint = value} -> getPrintAsm value initialAsm
--- src/Generator/X86Assembly.hs
+++ src/Generator/X86Assembly.hs
@@ -1,12 +1,33 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use newtype instead of data" #-}
 module Generator.X86Assembly where
-import Foreign (touchForeignPtr)
 
-data Register = RDI | RSI deriving Show
+import Foreign (touchForeignPtr)
+import Text.Printf
+
+data Register = RDI | RSI 
+
+instance Show Register where 
+    show RDI = "rdi"
+    show RSI = "rsi"
+
 
 data X86Data = X86Data {variableName :: String, value :: String, end :: Integer} deriving Show
-data X86Instruction = MOV Register String | Call String deriving Show
+
+data X86Instruction = MOV Register String | Call String | 
+        Extern String | Global String | Default String | StringPair String String
+
+instance Show X86Instruction where
+    show (MOV reg value) = printf "mov %s, %s" (show reg) value
+    show (Call value) = printf "call %s" value
+    show (Extern value) = printf "extern %s" value
+    show (Default value) = printf "default %s" value
+    show (Global value) = printf "global %s" value
+    show (StringPair val1 val2) = printf "%s %s" val1 val2
 
 data X86Assembly = X86Assembly {codeSection :: [X86Instruction], dataSection :: [X86Data] } deriving Show
 
+mergeAsm :: X86Assembly -> X86Assembly -> X86Assembly 
+mergeAsm (X86Assembly {codeSection = codeSectionOld, dataSection = dataSectionOld}) 
+    (X86Assembly {codeSection = codeSectionNew, dataSection = dataSectionNew}) = X86Assembly 
+        {codeSection = codeSectionOld ++ codeSectionNew, dataSection = dataSectionOld ++ dataSectionNew }
